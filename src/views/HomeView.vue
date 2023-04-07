07b4160c8640ffab1533f477a2de43c2a2349c19
denikaev_er <denikaev_er@itms.tech>
2023-04-08 01:08:32 +0400

    init project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+  <vm-card-header>
+    <vm-button primaryColor="red" routerPath="/about"> Открыть </vm-button>
+  </vm-card-header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+});
+</script>
