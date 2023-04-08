70331bd7c7a275d6bdf0d69a6e5eb50567c28da6
denikaev_er <denikaev_er@itms.tech>
2023-04-08 16:28:16 +0400

    add main page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div>
+    <nav>
+      <div class="header header-first">
+        <a class="logo">RUSFUTURE</a>
+        <div class="info">
+          <a class="slogan">ОЦЕНИТЕ СВОИ УСПЕХИ </a>
+          <a class="slogan-second">С ПОМОЩЬЮ РЕЙТИНГА</a>
+        </div>
+      </div>
+      <div class="header">
+        <router-link to="/" class="header-main">Главная</router-link>
+        <router-link to="/about">Аналитика</router-link>
+      </div>
+    </nav>
+    <router-view />
+    <nav class="footer">
+      <p>© 2023 RUSFUTURE - All Rights Reserved</p>
+    </nav>
+  </div>
 </template>
 
 <style lang="scss">
@@ -12,19 +26,65 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  font: 16px/22px Roboto, sans-serif;
+  color: #202020;
+  background-color: #fbfbfb;
+}
+
+.header {
+  display: flex;
+  align-items: center;
+  margin-right: 40px;
+  margin-left: 40px;
+  &-main {
+    margin-right: 40px;
+  }
 }
 
 nav {
   padding: 30px;
-
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  :hover {
+    color: #cecece;
+  }
   a {
     font-weight: bold;
-    color: #2c3e50;
-
+    color: #202020;
     &.router-link-exact-active {
-      color: #42b983;
+      color: #6eb4fa;
     }
+  }
+  a.logo {
+    color: #202020;
+    margin-right: 40px;
+    :hover {
+      color: #cecece;
+    }
+  }
+  .info {
+    display: flex;
+    flex-direction: column;
+    a.slogan {
+      color: #6eb4fa;
+      &-second {
+        color: white;
+        background: #6eb4fa;
+        border-radius: 5px;
+        padding: 4px;
+      }
+    }
+  }
+}
+nav.footer {
+  display: flex;
+  justify-content: space-around;
+}
+@media (max-width: 585px) {
+  .header-first {
+    display: none;
   }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div class="container">
+      <img
+        src="../assets/rating.jpeg"
+        alt="Информация"
+        class="py-7 py-md-0 bg-hero image"
+      />
+      <router-link to="/about">
+        <button class="btn">Аналитика</button>
+      </router-link>
+    </div>
+    <HelloWorld />
   </div>
-  <vm-card-header>
-    <vm-button primaryColor="red" routerPath="/about"> Открыть </vm-button>
-  </vm-card-header>
 </template>
 
 <script lang="ts">
@@ -19,3 +25,66 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="scss">
+.image {
+  width: 100vw;
+  box-sizing: border-box;
+  overflow: hidden;
+}
+
+.container {
+  position: relative;
+  width: 50vw;
+}
+
+/* Make the image responsive */
+.container img {
+  width: 100vw;
+  height: auto;
+}
+
+/* Style the button and place it in the middle of the container/image */
+.container .btn {
+  position: absolute;
+  top: 60%;
+  left: 50vw;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  background-color: #6eb4fa;
+  color: white;
+  font-size: 16px;
+  padding: 20px 40px;
+  border: none;
+  cursor: pointer;
+  border-radius: 10px;
+}
+
+.container .main {
+  position: absolute;
+  top: 40%;
+  width: 100%;
+  left: 50vw;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  color: #202020;
+  text-shadow: 1px 1px 0 #6eb4fa;
+}
+
+.container .btn:hover {
+  color: #cecece;
+}
+
+@media (max-width: 1100px) {
+  .container .main {
+    top: 20%;
+    font-size: 16px;
+  }
+}
+
+@media (max-width: 567px) {
+  .container .main {
+    display: none;
+  }
+}
+</style>
